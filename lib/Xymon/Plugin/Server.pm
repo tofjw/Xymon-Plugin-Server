50a45dd21e55710341de93a47febabd1721382ff
tofjw <tttfjw@gmail.com>
2016-11-18 21:57:30 +0900

    change version number for release

--- lib/Xymon/Plugin/Server.pm
+++ lib/Xymon/Plugin/Server.pm
@@ -11,11 +11,11 @@ Xymon::Plugin::Server - Xymon Server side plugin helper
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 DESCRIPTION
